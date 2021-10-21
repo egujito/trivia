d20b8a96597dcf42db45bfcab9ef76bc57fb861b
saiko-2043 <bessapachecolr@gmail.com>
2021-10-21 11:11:10 +0100

    updated theme to Dracula color pallete. Solved bug on mobile devices.

--- js/quiz.js
+++ js/quiz.js
@@ -6,10 +6,10 @@ function getdata(once, ops = null) {
 
     if (once) {
         for (let i = 0; i < 4; ++i) {
-            ops[i].style.background = "turquoise";
+            ops[i].style.background = "#BD93F9";
         }
     }
-    
+
     var targetURL = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple"
     var request = new XMLHttpRequest();
 
@@ -53,13 +53,13 @@ function addEventstoButtons(structure, ops) {
     ops[0].addEventListener("click", () => {
         if (!c) {
             if (ops[0].textContent == structure['results'][0]['correct_answer']) {
-                ops[0].style.background = "#ADFF2F";
+                ops[0].style.background = "#42E66C";
                 c = true;
                 showCorrectOnes(structure, ops);
             }
             else {
                 c = true;
-                showCorrectOnes(structure, ops);
+                showCorrectOnes(structure, ops, 0);
             }
         }
     });
@@ -68,13 +68,13 @@ function addEventstoButtons(structure, ops) {
         if (!c) {
 
             if (ops[1].textContent == structure['results'][0]['correct_answer']) {
-                ops[1].style.background = "#ADFF2F";
+                ops[1].style.background = "#42E66C";
                 c = true;
                 showCorrectOnes(structure, ops);
             }
             else {
                 c = true;
-                showCorrectOnes(structure, ops);
+                showCorrectOnes(structure, ops, 1);
             }
         }
     });
@@ -83,13 +83,13 @@ function addEventstoButtons(structure, ops) {
         if (!c) {
 
             if (ops[2].textContent == structure['results'][0]['correct_answer']) {
-                ops[2].style.background = "#ADFF2F";
+                ops[2].style.background = "#42E66C";
                 c = true;
                 showCorrectOnes(structure, ops);
             }
             else {
                 c = true;
-                showCorrectOnes(structure, ops);
+                showCorrectOnes(structure, ops, 2);
             }
         }
     });
@@ -98,30 +98,32 @@ function addEventstoButtons(structure, ops) {
         if (!c) {
 
             if (ops[3].textContent == structure['results'][0]['correct_answer']) {
-                ops[3].style.background = "#ADFF2F";
+                ops[3].style.background = "#42E66C";
                 c = true;
                 showCorrectOnes(structure, ops);
             }
             else {
                 c = true;
-                showCorrectOnes(structure, ops);
+                showCorrectOnes(structure, ops, 3);
             }
         }
     });
 }
 
-function showCorrectOnes(structure, ops) {
+function showCorrectOnes(structure, ops, try_index=10) {
     for (var i = 0; i < 4; ++i) {
         if (ops[i].textContent != structure['results'][0]["correct_answer"]) {
             for (var j = 0; j < 4; ++j) {
-                if (ops[i].textContent == structure['results'][0]["incorrect_answers"][j]) {
-                    ops[i].style.background = "red";
-
+                if (ops[i].textContent == structure['results'][0]["incorrect_answers"][j] && ops[i] == ops[try_index]) {
+                    ops[i].style.background = "#E4F34A";
+                }
+                else if (ops[i].textContent == structure['results'][0]["incorrect_answers"][j] && ops[i] != ops[try_index]) {
+                    ops[i].style.background = "#FF5555";
                 }
             }
         }
         else {
-            ops[i].style.background = "#ADFF2F";
+            ops[i].style.background = "#42E66C";
         }
     }
     reset(ops);
